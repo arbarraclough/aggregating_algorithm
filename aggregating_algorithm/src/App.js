cba64a943cf71f79672940e98fb3553b5a394a55
Andrew Barraclough <andrew.robert.barraclough@gmail.com>
2024-07-11 17:11:44 +0100

    Project: Fix aggregatingAlgorithm Method

--- aggregating_algorithm/src/App.js
+++ aggregating_algorithm/src/App.js
@@ -1,181 +1,250 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { aggregatingAlgorithm, generateExperts, getAwakeExperts, getAwakeExpertsWithConstantlyAwakeExperts, castLearnerPredictions, castExpertsPredictions } from './utils';
-import { Line } from 'react-chartjs-2';
-import 'chart.js/auto';
-import axios from 'axios';
-import Expert from './Expert.js';
 
-const AGGREGATING_ALGORITHM_BOUND = -(1.0 / 2.0) * Math.log(4);
-const FILENAME = 'sleeping_expert_test.txt';
+const MAX_PREFIX_LENGTH = 3;
+const TEST_SEQUENCE = "0110101000110100110110101011110000110011010111110011110110011010000011101111101111100111100110101011011101101110001010110100011000010110011000000000000000001000111101011010100010100001000000100001110001001010100101110101101100100110001000001110110011001101010110100111010100111011001111011001111010011111101101000110111010001111000000000001000100010000111100111100100100010001101100011101010001010101011001000001010000011010110000001010110011101011101010110100100001011001000001010100001101111111100010101111111001000011000011110101000001000011001000001001001001011011110111110100100111100100000110000100001100001100100100000110101101110100101110000100001010000011000010001111011001110011110000010111101100100100110100100100010000011111101111111110101010110010011000100001110000010011100110111011011111100011110111011110100011000000101010011000110010111100101011111001000110111101001110110011101011110101110000000100101111011100110001000101111100111100101001100000001001010100100000110110001101001110";
+const MAX_SEQUENCE_LENGTH = 15;
 
-const App = () => {
-  const [fileContent, setFileContent] = useState('');
-  const [gammas, setGammas] = useState([]);
-  const [omegas, setOmegas] = useState([]);
-  const [expertsPredictions, setExpertsPredictions] = useState([]);
-  const [learnerPredictions, setLearnerPredictions] = useState([]);
-  const [cumulativeExpertsLosses, setCumulativeExpertsLosses] = useState([]);
-  const [cumulativeLearnerLoss, setCumulativeLearnerLoss] = useState([]);
+const Expert = ({ prefix, binaryInput }) => {
+  let [numZeros, setNumZeros] = useState(0.0);
+  let [numOnes, setNumOnes] = useState(0.0);
+  let [awake, setAwake] = useState(false)
+  let lastBit = ''
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(FILENAME);
-        const fileText = response.data;
-        setFileContent(fileText);
-
-        const lines = fileText.split('\n');
-        const gammasData = [];
-
-        lines.forEach(line => {
-          const columns = line.trim().split(/\s+/);
-          if (columns.length > 3) {
-            const values = columns.slice(3).filter((_, index) => index % 2 === 0).map(Number);
-            gammasData.push(values);
-          }
-        });
-
-        setGammas(gammasData);
-        setOmegas(new Array(gammasData.length).fill(1));
-
-        const [
-          expertsPreds,
-          learnerPreds,
-          cumulativeExpertsLosses,
-          cumulativeLearnerLoss
-        ] = aggregatingAlgorithm(gammasData, new Array(gammasData.length).fill(1));
-
-        setExpertsPredictions(expertsPreds);
-        setLearnerPredictions(learnerPreds);
-        setCumulativeExpertsLosses(cumulativeExpertsLosses);
-        setCumulativeLearnerLoss(cumulativeLearnerLoss);
-      } catch (error) {
-        console.error('Error fetching the file:', error);
+      if (!binaryInput || binaryInput === '') {
+        setAwake(false);
+        return;
       }
-    };
 
-    fetchData();
-  }, []);
-  
-    const createLossChartData = () => {
-      const labels = Array.from({ length: gammas.length + 1 }, (_, i) => i);
-      const datasets = cumulativeExpertsLosses[0].map((_, i) => ({
-        label: `Loss_E_${i + 1}(t)`,
-        data: cumulativeExpertsLosses.map(loss => loss[i]),
-        borderColor: `rgba(${(i * 50) % 255}, ${(i * 80) % 255}, ${(i * 110) % 255}, 0.7)`,
-        fill: false
-      }));
-  
-      datasets.push({
-        label: 'Loss_L(t)',
-        data: cumulativeLearnerLoss,
-        borderColor: 'black',
-        fill: false
-      });
-  
-      return { labels, datasets };
-    };
-  
-    const createDifferenceChartData = () => {
-      const labels = Array.from({ length: gammas.length + 1 }, (_, i) => i);
-      const datasets = cumulativeExpertsLosses[0].map((_, i) => ({
-        label: `Loss_E_${i + 1}(t) - Loss_L(t)`,
-        data: cumulativeExpertsLosses.map(loss => loss[i] - cumulativeLearnerLoss[loss.length - 1]),
-        borderColor: `rgba(${(i * 50) % 255}, ${(i * 80) % 255}, ${(i * 110) % 255}, 0.7)`,
-        fill: false
-      }));
-  
-      datasets.push({
-        label: 'Aggregating Algorithm Bound',
-        data: Array(labels.length).fill(AGGREGATING_ALGORITHM_BOUND),
-        borderColor: 'black',
-        borderDash: [10, 5],
-        fill: false
-      });
-  
-      return { labels, datasets };
-    };
+      try {
+        lastBit = binaryInput.binaryInput.slice(-1);
+        console.log(`lastBit: ${lastBit}`);
+        /** If the expert was previously awake, increment the relevant counter. */
+        if (awake) {
+          (lastBit == 0) ? setNumZeros(numZeros + 1) : setNumOnes(numOnes + 1);
+        }
+
+        /** Check if the expert is awake. */
+        if (binaryInput.binaryInput.slice(-prefix.length) === prefix) {
+          setAwake(true);
+        } else {
+          setAwake(false);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+  }, [binaryInput, prefix])
+
+  const toString = () => {
+      return `Expert "${prefix}" - (${numZeros}, ${numOnes})`;
+  };
+
+ const predict = () => {
+    if (awake) {
+      if (numZeros + numOnes === 0) {
+        return 0;
+      }
+      return (numOnes / (numZeros + numOnes)).toFixed(4);
+    } else {
+      return '-';
+    }
+  };
 
   return (
-    <div>
-      <h1>Aggregating Algorithm Output</h1>
-      <pre>{fileContent}</pre>
+      <tr>
+          <td>{toString()}</td>
+          <td>{awake ? "Yes" : "No"}</td>
+          <td>{predict()}</td>
+      </tr>
+  );
+};
 
-      {gammas.length > 0 && (
-        <>
-          <h2>Cumulative Losses vs. Time</h2>
-          <Line data={createLossChartData()} />
-        {/* 
-          <h2>Differences of Cumulative Losses vs. Time</h2>
-          <Line data={createDifferenceChartData()} />
-        */}
-        </>
-      )}
+function App() {
+  // const [gammas, setGammas] = useState([]);
+  let [omegas, setOmegas] = useState([]);
+  const [expertsPredictions, setExpertsPredictions] = useState([]);
+  // const [learnerPredictions, setLearnerPredictions] = useState([]);
+  // const [cumulativeExpertsLosses, setCumulativeExpertsLosses] = useState([]);
+  // const [cumulativeLearnerLoss, setCumulativeLearnerLoss] = useState([]);
+  let [binaryInput, setBinaryInput] = useState('');
+  let [experts, setExperts] = useState([]);
+  
+  React.useState(() => {
+    const generatedExperts = generateExperts(MAX_PREFIX_LENGTH);
+    setExperts(generatedExperts);
+    const AGGREGATING_ALGORITHM_BOUND = -(1.0 / 2.0) * Math.log(generateExperts.length);
+  }, [])
+  
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    if (/^[01]*$/.test(value) && value.length <= MAX_SEQUENCE_LENGTH) {
+      setBinaryInput(value);
+      setOmegas(omegas => [...omegas, value.slice(-1)]);
+    }
+    console.log(omegas)
+    let expertsPredictions = [];
+    console.log(experts)
+    for (let i = 0; i < experts.length; i++) {
+      const expert = experts[i];
+      // expertsPredictions.push(expert.predict());
+    }
+    console.log(expertsPredictions)
+  };
+  
+  const handleReset = () => {
+    setBinaryInput('');
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Binary Sequence Input</h1>
+      </header>
+      <div className="columns">
+        <div className="column">
+          <input
+            type="text"
+            value={binaryInput}
+            onChange={handleInputChange}
+            placeholder="Binary Sequence"
+            className="input-box"
+            maxLength={MAX_SEQUENCE_LENGTH}
+            />
+        </div>
+        <div className="column">
+          <p className="counter">{binaryInput.length} / {MAX_SEQUENCE_LENGTH}</p>
+        </div>
+        <div className="column">
+          <button onClick={handleReset} className="reset-button">Reset</button>
+        </div>
+      </div>
+      <p><b>Learner's Prediction:</b> {}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Expert</th>
+            <th>Awake?</th>
+            <th>Prediction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {experts.map((expert, index) => (
+            <Expert key={index} prefix={expert.prefix} binaryInput={{binaryInput}} />
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
+};
+
+export default App;
+
+// const AGGREGATING_ALGORITHM_BOUND = -(1.0 / 2.0) * Math.log(4);
+// const FILENAME = 'sleeping_expert_test.txt';
+
+// const App = () => {
+//   const [fileContent, setFileContent] = useState('');
+
+//   useEffect(() => {
+//     /** Asynchronous function to fetch data from a file. */
+//     const fetchData = async () => {
+//       try {
+//         /** Make a GET request to fetch the file. */
+//         const response = await axios.get(FILENAME);
+//         const fileText = response.data;
+//         setFileContent(fileText);
+//         /** Split the file content into lines. */
+//         const lines = fileText.split('\n');
+
+//         const gammas = [];
+
+//         lines.forEach(line => {
+//           /** Split each line into columns based on whitespace. */
+//           const columns = line.trim().split(/\s+/);
+//           const values = columns.slice(3).filter((_, index) => index % 2 === 0).map(Number);
+//           gammas.push(values)
+//         });
+
+//         setGammas(gammas);
+//         setOmegas(new Array(lines.length).fill(1));
+//       } catch (error) {
+//         console.error('Error fetching the file:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   useEffect(() => {
+//     if (gammas.length > 0 && omegas.length > 0) {
+//       const [
+//         expertsPredictions,
+//         learnerPredictions,
+//         cumulativeExpertsLosses,
+//         cumulativeLearnerLoss
+//       ] = aggregatingAlgorithm(gammas, omegas);
+
+//       /** Store the results from the Aggregating Algorithm in the state. */
+//       setExpertsPredictions(expertsPredictions);
+//       setLearnerPredictions(learnerPredictions);
+//       setCumulativeExpertsLosses(cumulativeExpertsLosses);
+//       setCumulativeLearnerLoss(cumulativeLearnerLoss);
+
+//       setLoading(false);
+//     }
+//   }, [gammas, omegas]);
+
+//   const renderTable = (data, headers) => (
+//     <table>
+//       <thead>
+//         <tr>
+//           {headers.map((header, index) => (
+//             <th key={index}>{header}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {data.map((row, rowIndex) => (
+//           <tr key={rowIndex}>
+//             {row.map((cell, cellIndex) => (
+//               <td key={cellIndex}>{cell.toFixed(4)}</td>
+//             ))}
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
 
 //   return (
 //     <div>
 //       <h1>Aggregating Algorithm Output</h1>
-//       {gammas.length > 0 && (
-//         <>
-//           <h2>Cumulative Losses vs. Time</h2>
-//           <Line data={createLossChartData()} />
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         gammas.length > 0 && omegas.length > 0 && (
+//           <>
+//             <h2>Experts Predictions</h2>
+//             {renderTable(expertsPredictions, gammas[0].map((_, index) => `Expert ${index + 1}`))}
 
-//           <h2>Differences of Cumulative Losses vs. Time</h2>
-//           <Line data={createDifferenceChartData()} />
-//         </>
+//             <h2>Learner Predictions</h2>
+//             {renderTable([learnerPredictions], gammas.map((_, index) => `${index + 1}`))}
+
+//             <h2>Cumulative Experts Losses</h2>
+//             {renderTable(cumulativeExpertsLosses, gammas[0].map((_, index) => `Expert ${index + 1}`))}
+
+//             <h2>Cumulative Learner Loss</h2>
+//             {renderTable([cumulativeLearnerLoss], gammas.map((_, index) => `${index + 1}`))}
+
+            
+//             <h2>Cumulative Losses vs. Time</h2>
+//             <Line data = {dat}>
+//             </Line>
+//           </>
+//         )
 //       )}
 //     </div>
-//   );
-};
-
-// function App() {
-//   const maxLength = 10;
-//   const [binaryInput, setBinaryInput] = useState('');
-
-//   const gammas = [
-//     [0.2, 0.4, 0.6],
-//   ]
-
-//   const handleInputChange = (e) => {
-//     const value = e.target.value;
-//     if (/^[01]*$/.test(value) && value.length <= maxLength) {
-//       setBinaryInput(value);
-//     }
-//   };
-
-//   const handleReset = () => {
-//     setBinaryInput('');
-//   };
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Binary Sequence Input</h1>
-//       </header>
-//       <div className="columns">
-//         <div className="column">
-//           <input
-//             type="text"
-//             value={binaryInput}
-//             onChange={handleInputChange}
-//             placeholder="10-digit Binary Sequence"
-//             className="input-box"
-//             maxLength={maxLength}
-//           />
-//         </div>
-//         <div className="column">
-//           <p className="counter">{binaryInput.length} / 10</p>
-//         </div>
-//         <div className="column">
-//           <button onClick={handleReset} className="reset-button">Reset</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
+//   )
 // };
-
-export default App;
--- aggregating_algorithm/src/utils.js
+++ aggregating_algorithm/src/utils.js
@@ -1,5 +1,3 @@
-// utils.js
-
 export const aggregatingAlgorithm = (
     gammas,
     omegas,
@@ -9,60 +7,92 @@ export const aggregatingAlgorithm = (
   ) => {
     const N = gammas[0].length;
     const T = gammas.length;
-  
+
     const expertsPredictions = [];
     const learnerPredictions = [];
     let expertsLosses = [new Array(N).fill(0)];
     const learnerLoss = new Array(T).fill(0);
     let cumulativeExpertsLosses = [new Array(N).fill(0)];
     const cumulativeLearnerLoss = new Array(T + 1).fill(0);
-  
+    
+    /** Step 1: Initialise weights, w^i_0 = q_i, i = 1, 2, ..., N */
     if (!weights) {
       weights = new Array(N).fill(1 / N);
     }
   
+    /** Step 2: Repeat for t \in T... */
     for (let t = 0; t < T; t++) {
-      const gammas_t = gammas[t];
+      /** Step 3: Read the experts' predictions \gamma^n_t of awake experts */
+      let gammas_t = gammas[t];
       expertsPredictions.push(gammas_t);
-  
+
+      /** Maintain a list of awake experts */
       const awakeExperts = gammas_t.map(g => !isNaN(g));
+      /** If there aren't any awake experts for a timestep, skip the iteration */
       if (!awakeExperts.some(a => a)) {
         continue;
       }
-  
+      gammas_t = gammas_t.filter((_, j) => awakeExperts[j]);
+
+      /** Step 4: Normalise the weights of awake experts */
       const normalisedWeights = weights
         .filter((_, i) => awakeExperts[i])
         .map(w => w / weights.filter((_, i) => awakeExperts[i]).reduce((a, b) => a + b, 0));
-  
-      const generalisedPrediction = -(C_eta / eta) * Math.log(
-        [0, 1].map(omega_t =>
-          normalisedWeights.reduce((sum, w, i) =>
-            sum + w * Math.exp(-eta * Math.pow(omega_t - gammas_t.filter((_, j) => awakeExperts[j])[i], 2))
-          , 0)
-        )
+
+      /** Step 5: Solve the system (\omega \in \Omega) */
+      let weightedPredictions_0 = [];
+      for (let i = 0; i < gammas_t.length; i++) {
+        let lambda = normalisedWeights[i] * Math.exp(-eta * Math.pow(gammas_t[i], 2));
+        weightedPredictions_0.push(lambda);
+      }
+      let g0 = -(C_eta / eta) * Math.log(
+        weightedPredictions_0
+          .reduce((sum, val) => sum + val, 0)
       );
-  
+
+      let weightedPredictions_1 = [];
+      for (let i = 0; i < gammas_t.length; i++) {
+        let lambda = normalisedWeights[i] * Math.exp(-eta * Math.pow(1 - gammas_t[i], 2));
+        weightedPredictions_1.push(lambda);
+      }
+      let g1 = -(C_eta / eta) * Math.log(
+        weightedPredictions_1
+          .reduce((sum, val) => sum + val, 0)
+      );
+
+      const generalisedPrediction = [g0, g1];
+      
+      /** Calculate the leaner's prediction */
       const gamma_t = (1 / 2) - ((generalisedPrediction[1] - generalisedPrediction[0]) / 2);
       learnerPredictions.push(gamma_t);
-  
+      
+      /** Step 6: Observe the outcome \omega_t */
       const omega_t = omegas[t];
-      const awakeLosses = gammas_t.filter((_, i) => awakeExperts[i]).map(g => Math.pow(omega_t - g, 2));
+      
+      /** Calculate the awake experts' losses */
+      const awakeLosses = gammas_t.map(g => Math.pow(omega_t - g, 2));
+
+      /** Calculate the learner's and asleep experts' losses */
       const loss = Math.pow(omega_t - gamma_t, 2);
       const losses = awakeExperts.map((awake, i) => (awake ? awakeLosses.shift() : loss));
   
       expertsLosses.push(losses);
       learnerLoss[t] = loss;
-      cumulativeExpertsLosses = expertsLosses.reduce((acc, cur) => {
-        acc.push(acc[acc.length - 1].map((sum, i) => sum + cur[i]));
-        return acc;
+      cumulativeExpertsLosses = expertsLosses.reduce((sum, val) => {
+        sum.push(sum[sum.length - 1].map((sum, i) => sum + val[i]));
+        return sum;
       }, [new Array(N).fill(0)]);
       cumulativeLearnerLoss[t + 1] = cumulativeLearnerLoss[t] + learnerLoss[t];
-  
+      
       weights = weights.map((w, i) =>
         awakeExperts[i] ?
+          /** Step 7: Update the awake experts' weights w^n_t = w^n_{t-1} e^{-\eta \lambda(\gamma^n_t, \omega)} */
           w * Math.exp(-eta * losses[i]) :
+          /** Step 8: Update the sleeping experts' weights w^n_t = w^n_{t-1} e^{\frac{-\eta\lambda(\gamma_t, \omega)}{C(\eta)}} */
           w * Math.exp((-eta * losses[i]) / C_eta)
       );
+
+      /** Renormalise the updated experts' weights. */
       const weightSum = weights.reduce((a, b) => a + b, 0);
       weights = weights.map(w => w / weightSum);
     }
