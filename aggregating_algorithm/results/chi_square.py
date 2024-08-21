b574c5b75c992727315491af103d61bf49b80ed6
Andrew Barraclough <andrew.robert.barraclough@gmail.com>
2024-08-21 17:10:54 +0100

    Project: Update chi_square Script

--- aggregating_algorithm/results/chi_square.py
+++ aggregating_algorithm/results/chi_square.py
@@ -4,6 +4,24 @@ import numpy as np
 import os
 import scipy.stats as stats
 
+def count_runs(sequence):
+    if not sequence:
+        return []
+    
+    run_lengths = []
+    current_run_length = 1
+    
+    for i in range(1, len(sequence)):
+        if sequence[i] == sequence[i - 1]:
+            current_run_length += 1
+        else:
+            run_lengths.append(current_run_length)
+            current_run_length = 1
+    
+    run_lengths.append(current_run_length)
+    
+    return run_lengths
+
 PREFIXES = []
 for length in range(1, 5):
     for i in range(2**length):
@@ -12,33 +30,38 @@ for length in range(1, 5):
 
 DIRECTORY = "data"
 
+EXPECTED_NUMBER_OF_HEADS = stats.binom.pmf(np.arange(0, 11), 10, 0.5) * 100
+EXPECTED_NUMBER_OF_RUNS = stats.binom.pmf(np.arange(1, 11) - 1, 9, 0.5) * 100
+THEORETICAL_LENGTH_OF_RUNS = {
+    1: 3072,
+    2: 1408,
+    3: 640,
+    4: 288,
+    5: 128,
+    6: 56,
+    7: 24,
+    8: 10,
+    9: 4,
+    10: 2
+}
+EXPECTED_LENGTH_OF_RUNS = [(THEORETICAL_LENGTH_OF_RUNS[i] / sum(THEORETICAL_LENGTH_OF_RUNS.values())) * 100 for i in np.arange(1, 11)]
+
 FILES = [file for file in os.listdir(DIRECTORY)]
 COMBINED_NUMBER_OF_HEADS = {i: 0 for i in range(11)}
 COMBINED_NUMBER_OF_RUNS = {i: 0 for i in range(1, 11)}
-COMBINED_LENGTH_OF_RUNS = {1: 863, 2: 180, 3: 57, 4: 25, 5: 8, 6: 7, 7: 5, 8: 5, 9: 1, 10: 9}
+COMBINED_LENGTH_OF_RUNS = {i: 0 for i in range(1, 11)}
 combined_length = 0
 
 for file in FILES:
     with open(f"{DIRECTORY}/{file}", "r") as f:
         data = json.load(f)
 
-    CUMULATIVE_EXPERTS_LOSSES = np.array(data["cumulativeExpertsLosses"][:-1])
-    CUMULATIVE_LEARNER_LOSS = np.array(data["cumulativeLearnerLoss"][:-1])
-
-    T = len(CUMULATIVE_LEARNER_LOSS)
-    NUM_EXPERTS = len(CUMULATIVE_EXPERTS_LOSSES[0])
-
     SEQUENCES = data["sequences"]
     combined_length += len(SEQUENCES)
     for sequence in SEQUENCES:
-        current_run_length = 1
-        for i in range(1, len(sequence)):
-            if sequence[i] == sequence[i - 1]:
-                current_run_length += 1
-            else:
-                COMBINED_LENGTH_OF_RUNS[current_run_length] += 1
-                current_run_length = 1
-        COMBINED_LENGTH_OF_RUNS[current_run_length] += 1
+        run_lengths = count_runs(sequence)
+        for length in run_lengths:
+            COMBINED_LENGTH_OF_RUNS[length] += 1
 
         heads = sequence.count("1")
         COMBINED_NUMBER_OF_HEADS[heads] += 1
@@ -77,13 +100,14 @@ OBSERVED_NUMBER_OF_RUNS = [
 ]
 OBSERVED_LENGTH_OF_RUNS = [(COMBINED_LENGTH_OF_RUNS[i] / sum(COMBINED_LENGTH_OF_RUNS.values())) * 100 for i in np.arange(1, 11)]
 
+print(OBSERVED_LENGTH_OF_RUNS)
+
 chi_square, p_value = stats.chisquare(OBSERVED_NUMBER_OF_HEADS, EXPECTED_NUMBER_OF_HEADS)
-print(f"\u03C72(10, N=175) = {chi_square}, p = {p_value}")
+print(f"\u03C72(10, N={sum(COMBINED_NUMBER_OF_HEADS.values())}) = {chi_square}, p = {p_value}")
 
 chi_square, p_value = stats.chisquare(OBSERVED_NUMBER_OF_RUNS, EXPECTED_NUMBER_OF_RUNS)
-print(f"\u03C72(9, N=175) = {chi_square}, p = {p_value}")
+print(f"\u03C72(9, N={sum(COMBINED_NUMBER_OF_RUNS.values())}) = {chi_square}, p = {p_value}")
 
 chi_square, p_value = stats.chisquare(OBSERVED_LENGTH_OF_RUNS, EXPECTED_LENGTH_OF_RUNS)
-print(f"\u03C72(9, N=1,160) = {chi_square}, p = {p_value}")
-
+print(f"\u03C72(9, N={sum(COMBINED_LENGTH_OF_RUNS.values())}) = {chi_square}, p = {p_value}")
 
