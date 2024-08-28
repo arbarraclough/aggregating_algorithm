4415c785e2ae6452945f133d318fb9ea3c805626
Andrew Barraclough <andrew.robert.barraclough@gmail.com>
2024-08-28 11:21:04 +0100

    Dissertation: Submission!

--- aggregating_algorithm/results/compare_results.py
+++ aggregating_algorithm/results/compare_results.py
@@ -2,7 +2,6 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import scipy.stats as stats
 
 DIRECTORY = "data"
 FILES = sorted([file for file in os.listdir(DIRECTORY)])
@@ -17,10 +16,15 @@ for i, file in enumerate(FILES):
 
     OUTCOMES = [int(item) for sublist in data["sequences"] for item in sublist][1:]
     RAW_LEARNER_PREDICTIONS = data["learnerPredictions"]
-    CASTED_LEARNER_PREDICTIONS = [round(prediction) for prediction in RAW_LEARNER_PREDICTIONS][:-1]
+    CASTED_LEARNER_PREDICTIONS = [
+        round(prediction) for prediction in RAW_LEARNER_PREDICTIONS
+    ][:-1]
     CASTED_LEARNER_PREDICTIONS.insert(0, 0)
 
-    COMPARISON = [outcome == prediction for outcome, prediction in zip(OUTCOMES, CASTED_LEARNER_PREDICTIONS)]
+    COMPARISON = [
+        outcome == prediction
+        for outcome, prediction in zip(OUTCOMES, CASTED_LEARNER_PREDICTIONS)
+    ]
 
     CORRECT_COUNT = sum(COMPARISON)
     INCORRECT_COUNT = len(COMPARISON) - CORRECT_COUNT
@@ -29,24 +33,27 @@ for i, file in enumerate(FILES):
     file_names.append(f"Subject {i + 1}\n({ACCURACY*100:.2f}%)")
     correct_counts.append(CORRECT_COUNT)
     incorrect_counts.append(INCORRECT_COUNT)
+    print(
+        f"{file}: {sum(COMPARISON)} Correct | {len(COMPARISON) - sum(COMPARISON)} Incorrect | {ACCURACY*100:.2f}%"
+    )
 
 x = np.arange(len(file_names))
-width = 0.4
+WIDTH = 0.4
 
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(10, 5))
 correct_bars = ax.bar(
-    x - width / 2,
+    x - WIDTH / 2,
     correct_counts,
-    width,
+    WIDTH,
     label="Correct $\gamma_t$",
     color="#C6C6C6",
     edgecolor="black",
     alpha=0.75,
 )
 incorrect_bars = ax.bar(
-    x + width / 2,
+    x + WIDTH / 2,
     incorrect_counts,
-    width,
+    WIDTH,
     label="Incorrect $\gamma_t$",
     color="#929292",
     edgecolor="black",
@@ -58,8 +65,4 @@ ax.set_xticks(x)
 ax.set_xticklabels(file_names, ha="center", fontsize=12)
 ax.grid(axis="y")
 ax.legend(fontsize=12)
-plt.savefig(f"prediction_results.jpg", format="jpg")
-# plt.show()
-
-    # print(f"{file}: {sum(COMPARISON)} Correct | {len(COMPARISON) - sum(COMPARISON)} Incorrect | ({round(sum(COMPARISON) / len(COMPARISON), 2) * 100}%)")
-
+plt.savefig("prediction_results.jpg", format="jpg")
